118c57bebdc023c370b1b0d21604c3e33265fd25
seantriplett <srtriplett@loyola.edu>
2024-02-14 13:58:16 -0500

    Submission of project

--- Main.java
+++ Main.java
@@ -1,20 +1,54 @@
-/** Programmers:  [your names here]
-* Course:  CS 212
-* Due Date:
-* Lab Assignment:
-* Problem Statement:
-* Data In:
-* Data Out:
-* Credits: [Is your code based on an example in the book, in class, or something else?
-*            Reminder: you should never take code from the Internet or another person
-*/
+//** Programmers:  Wil and Sean
+//* Course:  CS 212
+//* Due Date: feb. 14, 2024
+//* Lab Assignment: lab 4
+//* Problem Statement: see if spheres overlap
+//* Data In:none
+//* Data Out:if spheres overlap
+//* Credits: N/A
+//*/
 
-import java.util.Scanner;
+        import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-       System.out.println("Music of the Spheres");
+        //initializes new sphere with x, y , z, radius
+        Sphere sun = new Sphere(0.0,0.0,0.0, 43.26);
+        Sphere earth = new Sphere(26.0,42.0,79.0,0.395);
+        Sphere planetX = new Sphere(25.3, 43.1,77.8,1.4);
+        //prints out surface area
+        System.out.println("Surface area of the sun: "+sun.getSurfaceArea());
+        System.out.println("Surface area of the earth: "+earth.getSurfaceArea());
+        System.out.println("Surface are of planetX: "+planetX.getSurfaceArea());
+        //prints out volume
+        System.out.println("Volume of sun: "+sun.getVolume());
+        System.out.println("Volume of earth: "+earth.getVolume());
+        System.out.println("Volume of planetX: "+planetX.getVolume());
+        //checks to see if earth and sun collide
+        if(sun.overlap(earth)){
+            System.out.println("The sun and earth do not overlap.");
+        }
+        else{
+            System.out.println("The sun and earth overlap.");
+        }
+        //checks to see if sun and planet x collide
+        if(sun.overlap(planetX)){
+            System.out.println("The sun and planetX do not overlap.");
+        }
+        else{
+            System.out.println("The sun and planetX overlap.");
+        }
+        //checks to see if earth and planet x collide
+        if(earth.overlap(planetX)){
+            System.out.println("The planetX and earth do not overlap.");
+        }
+        else{
+            System.out.println("The planetX and earth overlap.");
+        }
 
 
     }
+    //
+
 }
+
--- Sphere.java
+++ Sphere.java
@@ -1,20 +1,76 @@
-
-/** Programmers:  [your names here]
-* Course:  CS 212
-* Due Date:
-* Lab Assignment:
-* Problem Statement:
-* Data In:
-* Data Out:
-* Credits: [Is your code based on an example in the book, in class, or something else?
-*            Reminder: you should never take code from the Internet or another person
-*/
-
+//** Programmers:  Wil and Sean
+//* Course:  CS 212
+//* Due Date: feb. 14, 2024
+//* Lab Assignment: lab 4
+//* Problem Statement: see if spheres overlap
+//* Data In:none
+//* Data Out:if spheres overlap
+//* Credits: N/A
+//*/
 
 class Sphere {
-    public static void main(String[] args) {
-       System.out.println("Spheres Go Here");
 
+    private double radius;
+    private double x;
+    private double y;
+    private double z;
 
+    //Default constructor
+    public Sphere() {
+        //Initialize default values
+        this.radius = 0.0;
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
     }
+    //Constructor
+    public Sphere(double r, double x, double y, double z) {
+        //Initialize radius and coordinates based on user input
+        this.radius = r;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getSurfaceArea() {
+        //Calculate surface area of the sphere and return
+        return 4 * Math.PI * Math.pow(this.radius, 2);
+    }
+    public double getVolume() {
+        //Calculate the volume of the sphere and return
+        return (4/3) * Math.PI * Math.pow(this.radius, 3);
+    }
+    public double getDist(double x, double y, double z) {
+        //Calculate the distance of current sphere and point
+        return Math.sqrt(Math.pow((this.x-x), 2) + Math.pow((this.y-y), 2) + Math.pow((this.z-z), 2));
+    }
+
+    public boolean overlap(Sphere s){
+        //finds distance between 2 spheres
+        double totalDistance = getDist(s.x,s.y,s.z);
+        //finds the combined value of radius
+        double totalRadius = this.radius+s.radius;
+        //checks to see if distance between 2 planets is greater than combined radius'
+        if(totalDistance>totalRadius){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public double getX() {
+        return x;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+
+
 }
